cd4f90d0cb34b7a4aeea5c4b29db032982a931c8
Ekramul Haque <ehr.ekramul@gmail.com>
2025-01-11 14:31:32 +0600

    Initialize weather monitoring app dart project

---
+++ weather_monitoring_application/test/weather_monitoring_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:weather_monitoring_application/weather_monitoring_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
