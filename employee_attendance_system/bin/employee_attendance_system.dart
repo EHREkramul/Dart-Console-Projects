e0984bfc41be7f35b81e13a0595f91fd8a50e397
Ekramul Haque <ehr.ekramul@gmail.com>
2025-01-15 15:22:50 +0600

    Initialize Employee Attendance System

---
+++ employee_attendance_system/bin/employee_attendance_system.dart
@@ -0,0 +1,5 @@
+import 'package:employee_attendance_system/employee_attendance_system.dart' as employee_attendance_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${employee_attendance_system.calculate()}!');
+}
---
+++ employee_attendance_system/test/employee_attendance_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:employee_attendance_system/employee_attendance_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
