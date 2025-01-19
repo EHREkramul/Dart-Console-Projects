5548ca25b1065a7c2a58a7a4f4e051c12b5b1924
Ekramul Haque <ehr.ekramul@gmail.com>
2025-01-19 16:57:43 +0600

    Initialize Bank Transaction System

---
+++ bank_transaction_system/bin/bank_transaction_system.dart
@@ -0,0 +1,5 @@
+import 'package:bank_transaction_system/bank_transaction_system.dart' as bank_transaction_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bank_transaction_system.calculate()}!');
+}
---
+++ bank_transaction_system/test/bank_transaction_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:bank_transaction_system/bank_transaction_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
